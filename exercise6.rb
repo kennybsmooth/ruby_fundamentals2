18a0b0c0dddb27fb3e12004a7f918d9f49de8ab7
Kenny Bui <kennybui@Kennys-MacBook-Air.local>
2016-10-19 15:12:48 -0400

    Question 4 Complete Exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,1 +1,25 @@
 grocery_list = ["carrots", "crabs", "apples", "salmon"]
+#Q1 Add rice to the list using a shovel
+grocery_list << "rice"
+
+# Q1 Use an each statement to precede every item with "*"
+# Going to make it into its own array to use in Q5
+def asterisk(list)
+  list.each do |item|
+  puts "*" + "#{item}"
+  end
+end
+asterisk (grocery_list)
+
+# Q2 Check the number of items on your list using count
+puts "Your list has #{grocery_list.count.to_s} items"
+
+# Q3 Checking to see if bananas are on the list using include?
+if grocery_list.include?("bananas") == true
+  puts "you need to pick up bananas"
+else
+  puts "You dont need to get banana's today"
+end
+
+# Q4 Show 2nd item on the list by callist list[1] because it starts at zero
+puts "The 2nd item on the list is #{grocery_list[1]}"
